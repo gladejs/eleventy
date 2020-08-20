a9512b47d4093d755520111ee8af483c5d6e4729
Aurélien Stébé <aurelien.stebe@gmail.com>
2020-08-20 13:07:47 -0400

    feat(11ty): improve Eleventy's integration & config
    Replace the PassthroughAll option with a custom implementation,
    using "glob" & "fs-extra" to copy "Un11ty" files to the build folder.
    Promisify the whole process to do the copy while 11ty compiles
    the templates and make all file system operations async.
    Use the "incremental build" and load a default config file
    which activates the DataDeepMerge & sets proper folders.

--- dist/eleventy-rollup.js
+++ dist/eleventy-rollup.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const fs = require('fs')
+const path = require('path')
+const fs = require('fs-extra')
 const glob = require('glob')
 
 const Eleventy = require('@11ty/eleventy/src/Eleventy')
@@ -13,23 +14,55 @@ module.exports = function (input, output) {
     options (options) {
       const eleventy = new Eleventy(input, output)
 
-      eleventy.setIsVerbose(false) // Be quiet, no need to list all the files
-      eleventy.setPassthroughAll(true) // Rollup do need access to everything
+      eleventy.setIsVerbose(false) // be quiet, no need to list all the files
+      eleventy.setIncrementalBuild(true) // fewer builds in Live mode, please
 
-      return eleventy.init().then(async () =>
-        await (isLive ? eleventy.watch() : eleventy.write())
-      ).then(() => renameHTMLFiles(output)).then(() => options)
+      const configPath = require.resolve('./eleventy-config.js')
+      eleventy.setConfigPathOverride(path.relative('.', configPath))
+
+      return eleventyPromise(input, output, eleventy).then(() => options)
     }
   }
 }
 
-function renameHTMLFiles (rootDir) {
-  const files = glob.sync('**/*.html', {
-    cwd: rootDir, absolute: true
+function eleventyPromise (input, output, eleventy) {
+  return eleventy.init().then(() => {
+    const templates = eleventy.eleventyFiles.getGlobWatcherFiles()
+    const dataFiles = eleventy.eleventyFiles.getGlobWatcherTemplateDataFiles()
+
+    const writing = isLive ? eleventy.watch() : eleventy.write()
+    const copying = dataFiles.then(watchList =>
+      copyUn11tyFiles(input, output, watchList.concat(templates))
+    )
+
+    return Promise.all([copying, writing.then(() => renameHTMLFiles(output))])
+  })
+}
+
+function copyUn11tyFiles (rootDir, destDir, watchList) {
+  const files = glob.sync('*', {
+    nodir: true,
+    cwd: rootDir,
+    absolute: true,
+    matchBase: true,
+    ignore: watchList
   })
 
-  files.forEach(file => {
-    const base = file.substring(0, file.length - 4)
-    fs.renameSync(file, base + 'marko')
+  return Promise.all(files.map(file => {
+    const dest = destDir + file.substring(rootDir.length)
+    return fs.ensureDir(path.dirname(dest)).then(() => fs.copy(file, dest))
+  }))
+}
+
+function renameHTMLFiles (rootDir) {
+  const files = glob.sync('*.html', {
+    cwd: rootDir,
+    absolute: true,
+    matchBase: true
   })
+
+  return Promise.all(files.map(file => {
+    const base = file.substring(0, file.length - 4)
+    return fs.rename(file, base + 'marko')
+  }))
 }
