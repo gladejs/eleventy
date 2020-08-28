66546bdf12e17dd3bc14a0de635988781d59235e
Aurélien Stébé <aurelien.stebe@gmail.com>
2020-08-28 15:11:30 -0400

    feat(live): support Rollup Live mode ( "--watch" )
    Add 2 Chokidar instances in Live mode to watch
    for Un11ty files to copy and HTML files to rename.
    Also add a "fail-fast" check to init 11ty only once.

--- dist/eleventy-rollup.js
+++ dist/eleventy-rollup.js
@@ -8,11 +8,14 @@ const Eleventy = require('@11ty/eleventy/src/Eleventy')
 const isLive = process.env.ROLLUP_WATCH === 'true'
 
 module.exports = function (input, output) {
+  let eleventy = false
+
   return {
     name: 'eleventy',
 
     options (options) {
-      const eleventy = new Eleventy(input, output)
+      if (eleventy !== false) return options
+      eleventy = new Eleventy(input, output)
 
       eleventy.setIsVerbose(false) // be quiet, no need to list all the files
       eleventy.setIncrementalBuild(true) // fewer builds in Live mode, please
@@ -48,10 +51,19 @@ function copyUn11tyFiles (rootDir, destDir, watchList) {
     ignore: watchList
   })
 
-  return Promise.all(files.map(file => {
+  const copy = file => {
     const dest = destDir + file.substring(rootDir.length)
     return fs.ensureDir(path.dirname(dest)).then(() => fs.copy(file, dest))
-  }))
+  }
+
+  return Promise.all(files.map(copy)).then(() => {
+    if (isLive) {
+      const options = { ignoreInitial: true, ignored: watchList }
+      const watcher = require('chokidar').watch(rootDir, options)
+
+      watcher.on('add', copy).on('change', copy)
+    }
+  })
 }
 
 function renameHTMLFiles (rootDir) {
@@ -61,8 +73,17 @@ function renameHTMLFiles (rootDir) {
     matchBase: true
   })
 
-  return Promise.all(files.map(file => {
+  const move = file => {
     const base = file.substring(0, file.length - 4)
     return fs.rename(file, base + 'marko')
-  }))
+  }
+
+  return Promise.all(files.map(move)).then(() => {
+    if (isLive) {
+      const watcher = require('chokidar').watch(
+        rootDir + '/**/*.html', { ignoreInitial: true })
+
+      watcher.on('add', move).on('change', move)
+    }
+  })
 }
